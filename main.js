01dc937356307e19c63358dbc2896ac04b87b722
Franco Moure <128409278+FrancoMoure@users.noreply.github.com>
2023-11-13 21:40:28 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,99 @@
+/*
+	SELLING VIA EMAIL
+
+The listicle email template:
+
+identify the niche and specific product
+every point needs to have reasons
+emotions
+alegories
+
+but all of this is useless without this:
+more reasons...
+
+soft call to action
+
+what is this?
+why should I keep reading?
+what should I do next?
+
+
+get attention:
+challenge their beliefs
+ask a 2 option question
+be relatable
+ask a paradoxical question
+strong promise
+
+problem-solution loop
+
+give the solution at the end of the email and tell that that solution will come up with a new problem that can be fixed hitting the link
+
+------------------------------------
+	UX/UI
+
+UX = user experencie
+UI = user interface
+
+UX/UI Designer Marketplace = 85k / year
+
+roles: 
+visual/graphic designer: 
+ux designer: how users interact with the page
+ui designer: makes the experience pleasant
+interaction designer: 
+information architect: 
+content strategist: copywritters and other content
+
+	stages of WEB DEVELOPMENT
+DESIGN -> FRONTEND -> BACKEND
+
+DESIGN -> UX/UI
+ 
+Must answer these 4 questions:
+1-goals for the page (get downloads, get sales)
+2- users goals for the page (learn, get sth for free)
+3- questions that the user needs to answer in order to accomplish the goal
+4-emotions
+
+WIREFRAMING:
+
+This portion of the project consists of an initial sketch of where items will be placed on the various template pages.
+Templates for pages such as the blog, team, sidebar, full width and others will be sketched out.
+Color schemes will also be chosen at this point.
+Adobe XD is a powerful wireframing tool for UI/UX designers.
+-it helps creating mockups and wireframe layouts, create UI elements, and define user flowcharts,
+navigational structure and information architecture. (all in one design tool).
+
+FRONTEND
+html, css and js
+
+BACKEND
+where the data is stored
+
+
+Designer responsibilities:
+- User research - meaning I will be able to gather and evaluate user requirements in collaboration with managers
+and developers, stakeholders.
+- Ilustrate my ideas using storyboards, user flows, moodboards and sitemaps.
+- Design graphic user interface elements.
+- Develop mockups and prototypes that illustrate how sites function and look like.
+- Create additional graphic designs (banners, illustrations)
+- Identify and resolve UX problems.
+- Adjust mockups based on user feedback.
+- Develop and follow the standards in fronts, colors and elements.
+
+in my UX role I may be asked to:
+-Analyze statistical data (from Google Analytics for instance) or user feedback
+and propose solutions based on my findings.
+- Do competitive analysis.
+- Write a questionaire for retriving feedback in order to improve the current product.
+- Conduct an interview with the target audience.
+- Define personas of a project.
+- Create a sitemap.
+- Design user story and user flow.
+- Create a low-fidelity prototype with transactional screens.
+
+36:30 https://www.youtube.com/watch?v=uL2ZB7XXIgg
+
+*/
